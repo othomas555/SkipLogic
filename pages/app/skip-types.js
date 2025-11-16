5da7764c4d91bbe54b42b1d29d52e1dabd8de71c
othomas555 <o.thomas555@gmail.com>
2025-11-16 20:46:47 +0000

    Update skip-types.js

--- pages/app/skip-types.js
+++ pages/app/skip-types.js
@@ -48,7 +48,7 @@ export default function SkipTypesPage() {
       const { data: profile, error: profileError } = await supabase
         .from("profiles")
         .select("*")
-        .eq("user_id", user.id)
+        .eq("id", user.id)
         .single();
 
       if (profileError || !profile) {
