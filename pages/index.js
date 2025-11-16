348de10de3f79c931a84ee918b3b33cba1cea783
othomas555 <o.thomas555@gmail.com>
2025-11-16 18:13:50 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // pages/app/index.js
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 
 export default function AppDashboard() {
   const router = useRouter();
