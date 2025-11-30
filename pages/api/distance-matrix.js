bd41a921df924235a358598579765cfb8b6418d6
othomas555 <o.thomas555@gmail.com>
2025-11-30 14:13:00 +0000

    Update distance-matrix.js

--- pages/api/distance-matrix.js
+++ pages/api/distance-matrix.js
@@ -5,8 +5,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  try:
-  {
+  try {
     const { pairs } = req.body || {};
 
     // pairs = [{ key, from, to }, ...]
